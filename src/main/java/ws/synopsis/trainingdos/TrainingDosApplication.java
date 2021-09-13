a76a583789adc5cdb89ae852132e8a398eb66d50
Eklin Tarazona <eklin.tarazona@gmail.com>
2021-09-13 16:41:57 -0500

    first commit

---
+++ src/main/java/ws/synopsis/trainingdos/TrainingDosApplication.java
@@ -0,0 +1,13 @@
+package ws.synopsis.trainingdos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingDosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingDosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ws/synopsis/trainingdos/TrainingDosApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.synopsis.trainingdos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingDosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
