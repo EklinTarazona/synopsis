3e75cb1f3a4b22c3f366dd345235b4f392a3f5b8
Elvis Perez <ebardalesdj@mpfn.gob.pe>
2021-09-15 17:18:52 -0500

    Implementacion de log4j2

--- src/main/java/ws/synopsis/trainingdos/controller/v1/ClientController.java
+++ src/main/java/ws/synopsis/trainingdos/controller/v1/ClientController.java
@@ -1,5 +1,7 @@
 package ws.synopsis.trainingdos.controller.v1;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ws.synopsis.trainingdos.enumeration.TrainingStatusEnum;
@@ -16,11 +18,18 @@ import java.util.List;
 @RequestMapping("/client/v1")
 public class ClientController {
 
+    private static final Logger logger = LogManager.getLogger();
+
     @Autowired
     private ClientService service;
 
     @PostMapping
     public TrainingResponse<?> add(@RequestBody ClientRequest beanReq) throws TrainingException {
+        if(logger.isDebugEnabled()) {
+            logger.debug(String.format("Request: clientId(%s), name(%s)", beanReq.getClientId(), beanReq.getName()));
+            logger.debug("Ingreso a metodo add");
+            logger.debug("Ingreso a metodo add");
+        }
         service.add(beanReq);
         TrainingResponse<?> response = TrainingStatusEnum.OK.createResponse(Object.class);
         return response;
@@ -28,6 +37,12 @@ public class ClientController {
 
     @PutMapping
     public TrainingResponse<?> edit(@RequestBody ClientRequest beanReq) throws TrainingException {
+        if(logger.isInfoEnabled()) {
+            logger.info("Ingreso a metodo edit");
+            logger.info("Ingreso a metodo edit");
+            logger.info("Ingreso a metodo edit");
+            logger.info("Ingreso a metodo edit");
+        }
         service.edit(beanReq);
         TrainingResponse<?> response = TrainingStatusEnum.OK.createResponse(Object.class);
         return response;
@@ -35,6 +50,13 @@ public class ClientController {
 
     @DeleteMapping
     public TrainingResponse<?> remove(@RequestBody ClientDelRequest beanReq) {
+        if(logger.isWarnEnabled()) {
+            logger.warn("Ingreso a metodo remove");
+            logger.warn("Ingreso a metodo remove");
+            logger.warn("Ingreso a metodo remove");
+            logger.warn("Ingreso a metodo remove");
+            logger.warn("Ingreso a metodo remove");
+        }
         service.remove(beanReq);
         TrainingResponse<?> response = TrainingStatusEnum.OK.createResponse(Object.class);
         return response;
